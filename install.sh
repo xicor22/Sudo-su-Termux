9c5f751f8924cf50715c19bd1333a49798093cc0
Saksham Singh <51753814+xicor22@users.noreply.github.com>
2020-06-12 19:28:35 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+pkg install ncurses-utils
+wget https://raw.githubusercontent.com/xicor22/Sudo-su-Termux/master/sudo
+mv sudo /data/data/com.termux/files/usr/bin 
+chmod 700 /data/data/com.termux/files/usr/bin/sudo 
