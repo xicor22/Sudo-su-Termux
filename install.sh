f96b9782efd92b23b0b2d987aa2d9a7ed1edf1cc
Saksham Singh <51753814+xicor22@users.noreply.github.com>
2020-06-12 19:39:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,1 @@
-pkg install ncurses-utils
-wget https://raw.githubusercontent.com/xicor22/Sudo-su-Termux/master/sudo
-mv sudo /data/data/com.termux/files/usr/bin 
-chmod 700 /data/data/com.termux/files/usr/bin/sudo 
+pkg install ncurses-utils && wget https://raw.githubusercontent.com/xicor22/Sudo-su-Termux/master/sudo && mv sudo /data/data/com.termux/files/usr/bin && chmod 700 /data/data/com.termux/files/usr/bin/sudo 
